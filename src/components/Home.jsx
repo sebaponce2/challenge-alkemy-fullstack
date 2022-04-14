44fd3c621b81848b0f1be72d27caca2fe4bd217f
Sebastián Martin Ponce <sebaottok@gmail.com>
2022-04-14 19:31:33 -0300

    add: react icons: delete icon and change icon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,6 @@
+// component imports
+import { MdDelete } from "react-icons/md";
+import { FiEdit } from "react-icons/fi";
 // import { useState } from "react";
 
 const Home = () => {
@@ -14,53 +17,93 @@ const Home = () => {
                 </div>
                 <hr className="w-100 font-black my-0"/>
                 <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
-                    <p className="fw-bold my-auto my-auto font-green">Ingreso</p>
-                    <p className="fw-bold my-auto my-auto py-2">$3000</p>
+                    <p className="w-33 fw-bold my-auto my-auto font-green">Ingreso</p>
+                    <div className="d-flex w-33 justify-content-center">
+                        <p className="fs-4 my-auto pb-2 mx-3 delete"><MdDelete/></p>
+                        <p className="fs-4 my-auto pb-2 mx-3 change"><FiEdit/></p>
+                    </div>
+                    <p className="fw-bold my-auto w-33 text-end my-auto py-2">$3000</p>
                 </div>
                 <hr className="w-100 font-black my-0"/>
                 <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
-                    <p className="fw-bold my-auto font-green">Ingreso</p>
-                    <p className="fw-bold my-auto py-2">$5000</p>
+                    <p className="w-33 fw-bold my-auto font-green">Ingreso</p>
+                    <div className="d-flex w-33 justify-content-center">
+                        <p className="fs-4 my-auto pb-2 mx-3 delete"><MdDelete/></p>
+                        <p className="fs-4 my-auto pb-2 mx-3 change"><FiEdit/></p>
+                    </div>
+                    <p className="fw-bold my-auto w-33 text-end py-2">$5000</p>
                 </div>
                 <hr className="w-100 font-black my-0"/>
                 <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
-                    <p className="fw-bold my-auto py-2 font-red">Egreso</p>
-                    <p className="fw-bold my-auto py-2">$3000</p>
+                    <p className="w-33 fw-bold my-auto py-2 font-red">Egreso</p>
+                    <div className="d-flex w-33 justify-content-center">
+                        <p className="fs-4 my-auto pb-2 mx-3 delete"><MdDelete/></p>
+                        <p className="fs-4 my-auto pb-2 mx-3 change"><FiEdit/></p>
+                    </div>
+                    <p className="fw-bold my-auto w-33 text-end py-2">$3000</p>
                 </div>
                 <hr className="w-100 font-black my-0"/>
                 <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
-                    <p className="fw-bold my-auto font-green">Ingreso</p>
-                    <p className="fw-bold my-auto py-2">$3000</p>
+                    <p className="w-33 fw-bold my-auto font-green">Ingreso</p>
+                    <div className="d-flex w-33 justify-content-center">
+                        <p className="fs-4 my-auto pb-2 mx-3 delete"><MdDelete/></p>
+                        <p className="fs-4 my-auto pb-2 mx-3 change"><FiEdit/></p>
+                    </div>
+                    <p className="fw-bold my-auto w-33 text-end py-2">$3000</p>
                 </div>
                 <hr className="w-100 font-black my-0"/>
                 <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
-                    <p className="fw-bold my-auto py-2 font-red">Egreso</p>
-                    <p className="fw-bold my-auto py-2">$3000</p>
+                    <p className="w-33 fw-bold my-auto py-2 font-red">Egreso</p>
+                    <div className="d-flex w-33 justify-content-center">
+                        <p className="fs-4 my-auto pb-2 mx-3 delete"><MdDelete/></p>
+                        <p className="fs-4 my-auto pb-2 mx-3 change"><FiEdit/></p>
+                    </div>
+                    <p className="fw-bold my-auto w-33 text-end py-2">$3000</p>
                 </div>
                 <hr className="w-100 font-black my-0"/>
                 <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
-                    <p className="fw-bold my-auto font-green">Ingreso</p>
-                    <p className="fw-bold my-auto py-2">$3000</p>
+                    <p className="w-33 fw-bold my-auto font-green">Ingreso</p>
+                    <div className="d-flex w-33 justify-content-center">
+                        <p className="fs-4 my-auto pb-2 mx-3 delete"><MdDelete/></p>
+                        <p className="fs-4 my-auto pb-2 mx-3 change"><FiEdit/></p>
+                    </div>
+                    <p className="fw-bold my-auto w-33 text-end py-2">$3000</p>
                 </div>
                 <hr className="w-100 font-black my-0"/>
                 <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
-                    <p className="fw-bold my-auto py-2 font-red">Egreso</p>
-                    <p className="fw-bold my-auto py-2">$3000</p>
+                    <p className="w-33 fw-bold my-auto py-2 font-red">Egreso</p>
+                    <div className="d-flex w-33 justify-content-center">
+                        <p className="fs-4 my-auto pb-2 mx-3 delete"><MdDelete/></p>
+                        <p className="fs-4 my-auto pb-2 mx-3 change"><FiEdit/></p>
+                    </div>
+                    <p className="fw-bold my-auto w-33 text-end py-2">$3000</p>
                 </div>
                 <hr className="w-100 font-black my-0"/>
                 <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
-                    <p className="fw-bold my-auto font-green">Ingreso</p>
-                    <p className="fw-bold my-auto py-2">$3000</p>
+                    <p className="w-33 fw-bold my-auto font-green">Ingreso</p>
+                    <div className="d-flex w-33 justify-content-center">
+                        <p className="fs-4 my-auto pb-2 mx-3 delete"><MdDelete/></p>
+                        <p className="fs-4 my-auto pb-2 mx-3 change"><FiEdit/></p>
+                    </div>
+                    <p className="fw-bold my-auto w-33 text-end py-2">$3000</p>
                 </div>
                 <hr className="w-100 font-black my-0"/>
                 <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
-                    <p className="fw-bold my-auto py-2 font-red">Egreso</p>
-                    <p className="fw-bold my-auto py-2">$3000</p>
+                    <p className="w-33 fw-bold my-auto py-2 font-red">Egreso</p>
+                    <div className="d-flex w-33 justify-content-center">
+                        <p className="fs-4 my-auto pb-2 mx-3 delete"><MdDelete/></p>
+                        <p className="fs-4 my-auto pb-2 mx-3 change"><FiEdit/></p>
+                    </div>
+                    <p className="fw-bold my-auto w-33 text-end py-2">$3000</p>
                 </div>
                 <hr className="w-100 font-black my-0"/>
                 <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
-                    <p className="fw-bold my-auto font-green">Ingreso</p>
-                    <p className="fw-bold my-auto py-2">$3000</p>
+                    <p className="w-33 fw-bold my-auto font-green">Ingreso</p>
+                    <div className="d-flex w-33 justify-content-center">
+                        <p className="fs-4 my-auto pb-2 mx-3 delete"><MdDelete/></p>
+                        <p className="fs-4 my-auto pb-2 mx-3 change"><FiEdit/></p>
+                    </div>
+                    <p className="fw-bold my-auto w-33 text-end py-2">$3000</p>
                 </div>
             </div>
         </>

a5f6148747abd50b58cd49b98307cd25cad19996
Sebastián Martin Ponce <sebaottok@gmail.com>
2022-04-14 19:02:00 -0300

    add: colors css and responsive historial table

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,9 +1,14 @@
+// Component imports
 import Home from "./Home";
+
+// Css imports
+import "../css/Home.css";
+import "../css/Colors.css";
 
 const Container = () => {
     return ( 
         <>
-            <section>
+            <section className="container-page font-white">
                 <Home/>
             </section>
         </> 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,67 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 const Home = () => {
 
-    const [historial, setHistorial] = useState();
+    // const [historial, setHistorial] = useState();
     
     return (
         <>
-            <h1 className="text-center">Home</h1>
-            <div>
-                {
-                    historial.map((e) => {
-                        <div className="d-flex">
-                            <p>$3000</p>
-                            <p>Ingreso</p>
-                        </div>
-                    })
-                }
+            <h1 className="text-center py-4">Bienvenido, usuario!</h1>
+
+            <div className="bg-white table-p">
+                <div className="text-center py-4">
+                    <h2 className="text-cemy-auto nter font-black pb-2 pt-3 m-auto" style={{display: 'inline'}}>Dinero disponible: <h2 className="font-green" style={{display: 'inline'}}>$140000</h2></h2>
+                </div>
+                <hr className="w-100 font-black my-0"/>
+                <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
+                    <p className="fw-bold my-auto my-auto font-green">Ingreso</p>
+                    <p className="fw-bold my-auto my-auto py-2">$3000</p>
+                </div>
+                <hr className="w-100 font-black my-0"/>
+                <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
+                    <p className="fw-bold my-auto font-green">Ingreso</p>
+                    <p className="fw-bold my-auto py-2">$5000</p>
+                </div>
+                <hr className="w-100 font-black my-0"/>
+                <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
+                    <p className="fw-bold my-auto py-2 font-red">Egreso</p>
+                    <p className="fw-bold my-auto py-2">$3000</p>
+                </div>
+                <hr className="w-100 font-black my-0"/>
+                <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
+                    <p className="fw-bold my-auto font-green">Ingreso</p>
+                    <p className="fw-bold my-auto py-2">$3000</p>
+                </div>
+                <hr className="w-100 font-black my-0"/>
+                <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
+                    <p className="fw-bold my-auto py-2 font-red">Egreso</p>
+                    <p className="fw-bold my-auto py-2">$3000</p>
+                </div>
+                <hr className="w-100 font-black my-0"/>
+                <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
+                    <p className="fw-bold my-auto font-green">Ingreso</p>
+                    <p className="fw-bold my-auto py-2">$3000</p>
+                </div>
+                <hr className="w-100 font-black my-0"/>
+                <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
+                    <p className="fw-bold my-auto py-2 font-red">Egreso</p>
+                    <p className="fw-bold my-auto py-2">$3000</p>
+                </div>
+                <hr className="w-100 font-black my-0"/>
+                <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
+                    <p className="fw-bold my-auto font-green">Ingreso</p>
+                    <p className="fw-bold my-auto py-2">$3000</p>
+                </div>
+                <hr className="w-100 font-black my-0"/>
+                <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
+                    <p className="fw-bold my-auto py-2 font-red">Egreso</p>
+                    <p className="fw-bold my-auto py-2">$3000</p>
+                </div>
+                <hr className="w-100 font-black my-0"/>
+                <div className="d-flex justify-content-between font-black w-75 m-auto py-2">
+                    <p className="fw-bold my-auto font-green">Ingreso</p>
+                    <p className="fw-bold my-auto py-2">$3000</p>
+                </div>
             </div>
         </>
      );

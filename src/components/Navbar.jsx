c6e5d488cd05c645ec53f4779e257fe1fe0016c7
Sebastián Martin Ponce <sebaottok@gmail.com>
2022-04-17 00:03:41 -0300

    fix: grammar errors. add: name and last name in register section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
-// css imports
+// Component imports
+import { Link } from "react-router-dom";
+
+// Css imports
 import "../css/Navbar.css";
+
+
 const Navbar = () =>{
     return ( 
         <>
             <div className="navbar navbar-light bg-purple-dark px-3 fixed-navbar">
                 <div className="container-fluid d-flex justify-content-between container-navbar">
-                    <span className="navbar-brand mb-0 h1 font-poppins font-white fw-bold">Sebastián Ponce</span>
+                    <Link to="/" className="text-decoration-none"><p className="navbar-brand mb-0 h1 font-poppins font-white fw-bold">Sebastián Ponce</p></Link>
                     <div>
                         <ul className="navbar-nav">
                             <li className="nav-item font-poppins fw-bold">
-                                <a className="nav-link active font-white" aria-current="page" href="#">Log out</a>
+                                <Link className="nav-link active font-white" aria-current="page" to="/login">Login</Link>
                             </li>
                         </ul>
                     </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,17 @@
+// Component imports
 import { Link } from "react-router-dom";
+
 const Register = () => {
     return ( 
         <>
-            <section style={{height: '100vh'}}>
+            <section className="pb-5">
                 <div className="mt-10">
                     <form action="" className="login-form my-auto p-5">
-                    <h2 className="fw-bold text-center font-poppins font-black pb-4">Login</h2>
+                    <h2 className="fw-bold text-center font-poppins font-black pb-4">Register</h2>
+                        <label className="d-block fw-bold text-center font-poppins font-black mt-4 mb-2" htmlFor="name">Name</label>
+                        <input className="d-block w-100 p-2 px-4 font-poppins my-auto text-center" type="text" name="name" placeholder="Name"/>
+                        <label className="d-block fw-bold text-center font-poppins font-black mt-4 mb-2" htmlFor="lastName">Last name</label>
+                        <input className="d-block w-100 p-2 px-4 font-poppins my-auto text-center" type="text" name="lastName" placeholder="Last name"/>
                         <label className="d-block fw-bold text-center font-poppins font-black mt-4 mb-2" htmlFor="mail">Email</label>
                         <input className="d-block w-100 p-2 px-4 font-poppins my-auto text-center" type="email" name="mail" placeholder="Email"/>
                         <label className="d-block fw-bold text-center font-poppins font-black mt-4 mb-2" htmlFor="password">Password</label>
@@ -14,7 +20,6 @@ const Register = () => {
                         <input className="d-block w-100 p-2 px-4 font-poppins my-auto text-center" type="password" name="password" id="password" placeholder="Repeat your password"/>
                         <div className="text-center">
                             <Link to="/login"><button className="border-0 login-button fw-bold font-poppins">Register</button></Link>
-                           
                         </div>
                     </form>
                 </div>
